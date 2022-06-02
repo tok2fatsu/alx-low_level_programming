7480f295aa8bb831e50172484c36dc72baadb1be
tok2fatsu <mailit2moi@gmail.com>
2022-06-02 16:10:35 -0700

    printing exact string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - writes out the values
  * Description: avails the tru values out.
@@ -6,8 +7,7 @@
  */
 int main(void)
 {
-char str1[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+write(1,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",59);
 
-write(2, str1, 59);
 return (1);
 }
