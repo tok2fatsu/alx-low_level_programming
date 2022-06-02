eea970f99382b8bbcf96be051e85ab396c63c2ba
tok2fatsu <mailit2moi@gmail.com>
2022-06-02 14:16:36 -0700

    Print string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printf's a word
+ * Description: Longer description of the function
+ * Return: Description of the return bruv
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
