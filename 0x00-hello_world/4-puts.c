9a11cd6fb2e5ff77d055c44ea6971d7ddcbbf744
tok2fatsu <mailit2moi@gmail.com>
2022-06-02 14:11:17 -0700

    Print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -function
+ * Description: hope it works
+ * Return: returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
