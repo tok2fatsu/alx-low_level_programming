1a69535b181a52483ed114a54d5c6a092d4b0ffb
tok2fatsu <mailit2moi@gmail.com>
2022-06-02 14:18:14 -0700

    Print string

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Description: Longer description of the function
  * Return: Description of the return bruv
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
