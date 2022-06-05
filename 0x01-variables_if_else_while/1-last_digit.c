0b727c47f68d24605a7883db871f2d571c66ecea
tok2fatsu <mailit2moi@gmail.com>
2022-06-05 10:18:46 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,21 +2,23 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - generates random number n
- * Description: determines n is possessive, zero, or negative
- * Return: 0
- */
+* main - generates random number n
+* Description: determines whether n is 0, n > 5 or n < 6
+* Return: 0
+*/
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%i is positive\n", n);
-	else if (n == 0)
-		printf("%i is zero\n", n);
-	else
-		printf("%i is negative\n", n);
-	return (0);
+  int n;
+  int lastDigit;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  lastDigit = n % 10;
+  printf(“Last digit of %d is %d and is “, n, lastDigit);
+  if (lastDigit > 5)
+    printf(“greater than 5\n”);
+  else if (lastDigit == 0)
+    printf(“0\n”);
+  else
+    printf(“less than 6 and not 0\n”);
+  return (0);
 }
