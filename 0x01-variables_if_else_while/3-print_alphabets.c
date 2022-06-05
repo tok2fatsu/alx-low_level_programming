abf923856ba803b6515b60842450303d6f7270ab
tok2fatsu <mailit2moi@gmail.com>
2022-06-05 12:41:18 -0700

    print UC alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print out the alphabet
+ * Description: print alphabet in both lower and upper case
+ * Return:0
+ */
+int main(void)
+{
+	char a = 'a';
+		while (a <= 'z')
+		{
+			putchar(a);
+			a++;
+		}
+		a = 'A';
+			while (a <= 'Z')
+			{
+				putchar(a);
+				a++;
+			}
+			putchar('\n');
+		return (0);
+}
