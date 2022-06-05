2c44dec09244169a4bf50d3ae1017561b5ba0f25
tok2fatsu <mailit2moi@gmail.com>
2022-06-05 13:19:32 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Description: alphabet in reverse
+ * Return:0
+ */
+int main(void)
+{
+	char x = 'z';
+
+	while (x >= 'a')
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
