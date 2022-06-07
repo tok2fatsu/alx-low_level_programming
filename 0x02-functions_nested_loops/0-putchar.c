4e1a5734b7b6aa1058bdcfede1b56766378f2c99
tok2fatsu <mailit2moi@gmail.com>
2022-06-07 16:53:59 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar
+ * Description: printing a function as a string
+ * Return: 0
+ */
+int main()
+{
+	char ch[10] = "_putchar";
+	{
+	putchar(ch);
+	putchar('\n');
+	}
+return (0);
+}
+
