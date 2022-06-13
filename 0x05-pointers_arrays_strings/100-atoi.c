9b2866c03d95010b8d120582620f153d59ee0f04
tok2fatsu <mailit2moi@gmail.com>
2022-06-13 14:56:45 -0700

    Task 10: Converting string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"                                                                                                                                
+#include <stdio.h>                                                                                                                               
+                                                                                                                                                 
+/**                                                                                                                                              
+ * _atoi -starting point                                                                                                                         
+ *@s: input                                                                                                                                      
+ * Return: always 0                                                                                                                              
+ */                                                                                                                                              
+int _atoi(char *s)                                                                                                                               
+{                                                                                                                                                
+        int a = 1, b = 0;                                                                                                                        
+        unsigned int num = 0;                                                                                                                    
+                                                                                                                                                 
+for (b = 0; s[b] != '\0'; b++)                                                                                                                   
+{                                                                                                                                                
+        if (s[b] == '-')                                                                                                                         
+                a *= -1;                                                                                                                         
+                                                                                                                                                 
+        else if (s[b] >= 0 + '0' && s[b] < 10 + '0')                                                                                             
+                num = num * 10 + (s[b] - '0');                                                                                                   
+                                                                                                                                                 
+        else if (s[b - 1] >= 0 + '0' && s[b - 1] < 10 + '0')                                                                                     
+                break;                                                                                                                           
+}                                                                                                                                                
+                                                                                                                                                 
+return (num *a);                                                                                                                                 
+}               
