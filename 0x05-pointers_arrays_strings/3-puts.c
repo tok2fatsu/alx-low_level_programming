b993f1259503afec9c5c98d71d962ac4f33851ca
tok2fatsu <mailit2moi@gmail.com>
2022-06-13 13:36:51 -0700

    Task 3: Printing string + new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: string
+ * Return: void
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+
+	_putchar(*str);
+	str++;
+	}
+	_putchar('\n');
+}
