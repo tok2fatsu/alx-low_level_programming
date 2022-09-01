d7ac04bd9a21e96121ed561142debff34b828462
tok2fatsu <mailit2moi@gmail.com>
2022-09-01 06:33:42 -0700

    Task 0: A function that prints all the elements of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - printing function
+ * @h: header of file
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+	int iniprint = 0;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->prev != NULL && iniprint == 0)
+		{
+			h = h->prev;
+			continue;
+		}
+		else if (h->prev == NULL)
+		{
+			iniprint = 1;
+			i = 0;
+		}
+		printf("%i\n", h->n);
+		h= h->next;
+	}
+	return (i);
+}
